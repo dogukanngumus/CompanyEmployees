e000f3f28acc9181c39271b7ed9b8c8d4b4412c9
dogukanngumus <dogukanngumus@gmail.com>
2024-01-22 22:17:29 +0300

    first commit

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Contracts;
+using LoggerService;
+
+namespace CompanyEmployees.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services)
+    => services.AddCors(opt=>{
+        opt.AddPolicy("CorsPolicy",cfg=>{
+            cfg.AllowAnyHeader();
+            cfg.AllowAnyMethod();
+            cfg.AllowAnyOrigin();
+        });
+    });
+
+    public static void ConfigureIISIntegration(this IServiceCollection services)
+    => services.Configure<IISOptions>(opt=>{});
+
+    public static void ConfigureLoggerService(this IServiceCollection services)
+    => services.AddScoped<ILoggerManager, LoggerManager>();
+}
---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,27 @@
+using CompanyEmployees.Extensions;
+using CompanyEmployees.Presentation;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers().AddApplicationPart(typeof(AssemblyReference).Assembly);
+
+#region Configurations
+LogManager.Setup().LoadConfigurationFromFile();
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+#endregion
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions(){
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
+
+app.MapControllers();
+app.Run();
